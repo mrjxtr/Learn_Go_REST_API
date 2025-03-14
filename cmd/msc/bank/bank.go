60a196033add91544e1ea9893bdee26ddb7f4cc0
Jester Lumacad <mrjxtr.dev@gmail.com>
2025-03-14 23:36:32 +0800

    refactor: optimized bank.go logic

--- cmd/msc/bank/bank.go
+++ cmd/msc/bank/bank.go
@@ -3,50 +3,61 @@ package main
 import "fmt"
 
 func main() {
-	var choice int
+	fmt.Printf("WELCOME TO THE BANK!\n\n")
+
 	balance := 1000.0
+	wantsExit := false
 
-	fmt.Println("WELCOME TO THE BANK!")
-	fmt.Println("What do you want to do?")
-	fmt.Println("1. Check balance")
-	fmt.Println("2. Deposit Money")
-	fmt.Println("3. Withdraw Money")
-	fmt.Println("4. Exit")
+	for !wantsExit {
+		var choice int
+		var newBalance float64
 
-	fmt.Print("Your choice: ")
-	fmt.Scan(&choice)
+		fmt.Printf("What do you want to do?\n\n")
+		fmt.Println("1. Check balance")
+		fmt.Println("2. Deposit Money")
+		fmt.Println("3. Withdraw Money")
+		fmt.Println("4. Exit")
 
-	wantsExit := choice == 4
-	if !wantsExit {
+		fmt.Print("Your choice: ")
+		fmt.Scan(&choice)
+
 		if choice == 1 {
-			fmt.Println("Your balance is:", balance)
+			fmt.Printf("Your balance is: %v\n\n", balance)
 		} else if choice == 2 {
-			deposit := depositMoney()
+			var deposit float64
+
+			deposit, newBalance = depositMoney(balance)
+			balance = newBalance
 
 			fmt.Printf("You are depositing: %.2f\n", deposit)
-			fmt.Printf("Your new balance is : %.2f\n", deposit+balance)
+			fmt.Printf("Your new balance is : %.2f\n\n", newBalance)
 		} else if choice == 3 {
+			var withdrawAmount float64
 
-			withdrawAmount := withdrawMoney()
+			withdrawAmount, newBalance := withdrawMoney(balance)
+			balance = newBalance
 
 			fmt.Printf("You are withdrawing: %.2f\n", withdrawAmount)
-			fmt.Printf("Your new balance is : %.2f\n", balance-withdrawAmount)
+			fmt.Printf("Your new balance is : %.2f\n\n", newBalance)
+		} else {
+			wantsExit = true
+			fmt.Println("Exiting Bank, Thank you!")
 		}
-	} else {
-		fmt.Println("Exiting Bank, Thank you!")
 	}
 }
 
-func depositMoney() float64 {
+func depositMoney(balance float64) (float64, float64) {
 	var deposit float64
 	fmt.Print("How much do you want to deposit?: ")
 	fmt.Scan(&deposit)
-	return deposit
+	newBalance := deposit + balance
+	return deposit, newBalance
 }
 
-func withdrawMoney() float64 {
+func withdrawMoney(balance float64) (float64, float64) {
 	var withdrawAmount float64
 	fmt.Print("How much do you want to withdraw?: ")
 	fmt.Scan(&withdrawAmount)
-	return withdrawAmount
+	newBalance := balance - withdrawAmount
+	return withdrawAmount, newBalance
 }
