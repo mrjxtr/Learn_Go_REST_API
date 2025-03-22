44ae53250c9c43252533cdfcf379775fb1ea8354
Jester Lumacad <mrjxtr.dev@gmail.com>
2025-03-22 21:11:58 +0800

    update: error handling + writting to file exercise

--- cmd/msc/profit/profit_calculator.go
+++ cmd/msc/profit/profit_calculator.go
@@ -1,17 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main() {
-	revenue := getUserInput("Revenue: ")
-	expenses := getUserInput("Expenses: ")
-	taxRate := getUserInput("Tax Rate: ")
+	revenue, err := getUserInput("Revenue: ")
+	if err != nil {
+		fmt.Println("ERROR: Revenue", err)
+		return
+	}
+
+	expenses, err := getUserInput("Expenses: ")
+	if err != nil {
+		fmt.Println("ERROR: Expenses", err)
+		return
+	}
+
+	taxRate, err := getUserInput("Tax Rate: ")
+	if err != nil {
+		fmt.Println("ERROR: Tax Rate", err)
+		return
+	}
 
 	ebt, profit, ratio := calculateProfits(revenue, expenses, taxRate)
 
-	fmt.Printf("EBT: %.2f\n", ebt)
+	fmt.Printf("EBT: %.2f\n", ebt) // Earnings before Tax
+	storeResult(ebt, "ebt")
 	fmt.Printf("PROFIT: %.2f\n", profit)
+	storeResult(profit, "profit")
 	fmt.Printf("RATIO: %.2f\n", ratio)
+	storeResult(ratio, "ratio")
+
+	storeResults(ebt, profit, ratio)
 }
 
 func calculateProfits(rev, exp, tr float64) (float64, float64, float64) {
@@ -22,9 +45,28 @@ func calculateProfits(rev, exp, tr float64) (float64, float64, float64) {
 	return ebt, profit, ratio
 }
 
-func getUserInput(infoText string) float64 {
+func getUserInput(infoText string) (float64, error) {
 	var userInput float64
+
 	fmt.Print(infoText)
 	fmt.Scan(&userInput)
-	return userInput
+	if userInput <= 0 {
+		return 0, errors.New("input must be a positive number")
+	}
+
+	return userInput, nil
+}
+
+func storeResult(data float64, filename string) {
+	byteData := fmt.Sprintf("%.2f", data)
+	filePath := fmt.Sprintf("./cmd/msc/profit/%s.txt", filename)
+
+	os.WriteFile(filePath, []byte(byteData), 0644)
+}
+
+func storeResults(ebt, profit, ratio float64) {
+	byteData := fmt.Sprintf("EBT: %.2f\nPROFIT: %.2f\nRATION: %.2f", ebt, profit, ratio)
+	filePath := "./cmd/msc/profit/results.txt"
+
+	os.WriteFile(filePath, []byte(byteData), 0644)
 }
